e548e6c6b571a51fa03dcbff94c9374af47f576a
Bloom Perez <springdotgay@tuta.io>
2025-12-06 11:52:13 -0400

    add/update icons

--- apps/web/src/utils/icons.tsx
+++ apps/web/src/utils/icons.tsx
@@ -12,6 +12,16 @@ import {
   ServerMultipleRegular,
   ChatFilled,
   ChatRegular,
+  ChevronRightFilled,
+  ChevronRightRegular,
+  CheckmarkCircle24Filled,
+  CheckmarkCircle24Regular,
+  EditFilled,
+  EditRegular,
+  OpenFilled,
+  OpenRegular,
+  Attach24Filled,
+  Attach24Regular,
 } from "@fluentui/react-icons";
 
 const HomeIcon = bundleIcon(Home32Filled, Home32Regular);
@@ -20,12 +30,20 @@ const ChatMultipleIcon = bundleIcon(
   ChatMultiple24Filled,
   ChatMultiple24Regular
 );
+const AttachIcon = bundleIcon(Attach24Filled, Attach24Regular);
 const ChatIcon = bundleIcon(ChatFilled, ChatRegular);
 const CallIcon = bundleIcon(Call24Filled, Call24Regular);
 const ServerMultipleIcon = bundleIcon(
   ServerMultipleFilled,
   ServerMultipleRegular
 );
+const ChevronRightIcon = bundleIcon(ChevronRightFilled, ChevronRightRegular);
+const CheckmarkCircleIcon = bundleIcon(
+  CheckmarkCircle24Filled,
+  CheckmarkCircle24Regular
+);
+const EditIcon = bundleIcon(EditFilled, EditRegular);
+const OpenIcon = bundleIcon(OpenFilled, OpenRegular);
 
 export {
   HomeIcon,
@@ -34,4 +52,9 @@ export {
   CallIcon,
   ChatIcon,
   ServerMultipleIcon,
+  ChevronRightIcon,
+  CheckmarkCircleIcon,
+  EditIcon,
+  OpenIcon,
+  AttachIcon,
 };
